88f772c259fc4af5c8b72c437b7bad2781a74e20
Dragonmaster129 <dragons4756@gmail.com>
2021-09-07 11:10:47 -0600

    Adding slug url functionality

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default class NavigationContainer extends Component {
     constructor() {
@@ -12,11 +13,22 @@ export default class NavigationContainer extends Component {
     render() {
         return (
             <div>
-                <button>Home</button>
-                <button>About</button>
-                <button>Contact</button>
-                <button>Blog</button>
-                <button>Add Blog</button>
+                <NavLink exact to="/">
+                    Home
+                </NavLink>
+
+                <NavLink to="/about-me">
+                    About
+                </NavLink>
+
+                <NavLink to="/contact">
+                    Contact
+                </NavLink>
+
+                <NavLink to="/blog">
+                    Blog
+                </NavLink>
+                {true ? <button>Add Blog</button> : ""}
             </div>
         )
     }
---
+++ src/components/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PortfolioContainer from '../portfolio/portfolio-container';
+
+export default function() {
+    return (
+        <div>
+        <h1>HomePage</h1>
+        <PortfolioContainer />
+
+        </div>
+    );
+}
